ec45192519903f72c5fc5a07e79c1a8a0deac339
최근원 <kidjustinchoi@gmail.com>
2023-03-27 20:35:32 +0900

    feat: add text & weather info

--- src/App.tsx
+++ src/App.tsx
@@ -6,9 +6,15 @@ export default function App() {
   return (
     <View style={styles.container}>
       {/* <Text style={styles.text}>앙기모룽링</Text> */}
-      <View style={[styles.view, { backgroundColor: "tomato" }]}></View>
-      <View style={[styles.view, { backgroundColor: "teal", flex: 2 }]}></View>
-      <View style={[styles.view, { backgroundColor: "orange" }]}></View>
+      <View style={styles.city}>
+        <Text style={styles.cityName}>Seoul</Text>
+      </View>
+      <View style={styles.weather}>
+        <View style={styles.day}>
+          <Text style={styles.temp}>27</Text>
+          <Text style={styles.description}>Sunny</Text>
+        </View>
+      </View>
       <StatusBar style="light" />
     </View >
   );
@@ -21,13 +27,39 @@ export default function App() {
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1
+    flex: 1,
+    backgroundColor: '#FA647C'
   },
   text: {
     color: 'white',
     fontSize: 30,
   },
-  view: {
-    flex: 1
+  city: {
+    flex: 1.2,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  cityName: {
+    fontSize: 68,
+    fontWeight: 'bold',
+    color: '#fafafa',
+  },
+  weather: {
+    flex: 3,
+    // backgroundColor: "#5441d0",
+  },
+  day: {
+    flex: 1,
+    alignItems: 'center'
+  },
+  temp: {
+    marginTop: 50,
+    fontSize: 178,
+    color: '#fafafa',
+  },
+  description: {
+    fontSize: 60,
+    marginTop: -30,
+    color: '#fafafa',
   }
 });
