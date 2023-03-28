a9e5b6185f43d231fbb2faf5081bc62a8baf5196
choi138 <kidjustinchoi@gmail.com>
2023-03-28 18:42:25 +0900

    feat: get user location city

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,6 @@
+import * as Location from "expo-location";
 import { StatusBar } from "expo-status-bar";
+import { useEffect, useState } from "react";
 import { Dimensions } from "react-native";
 import * as S from "./styled";
 
@@ -6,10 +8,33 @@ const { width: SCREEN_WIDTH } = Dimensions.get("window");
 
 
 export default function App() {
+  const [city, setCity] = useState<string | null>("Loading...");
+  const [location, setLocation] = useState();
+  const [ok, setOk] = useState<boolean>(true);
+
+  const ask = async () => {
+    const { granted } = await Location.requestForegroundPermissionsAsync();
+    if (!granted) {
+      setOk(false);
+    }
+    const {
+      coords: { latitude, longitude }
+    } = await Location.getCurrentPositionAsync({ accuracy: 5 });
+    const location = await Location.reverseGeocodeAsync(
+      { latitude, longitude },
+      { useGoogleMaps: false }
+    );
+    setCity(location[0].city);
+  }
+
+  useEffect(() => {
+    ask();
+  }, [])
+
   return (
     <S.Container>
       <S.CityWrap >
-        <S.CityName >Seoul</S.CityName>
+        <S.CityName >{city}</S.CityName>
       </S.CityWrap>
       <S.WeatherWrap pagingEnabled horizontal showsHorizontalScrollIndicator={false}>
         <S.WeatherInfo screen_width={SCREEN_WIDTH}>
