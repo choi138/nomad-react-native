ab547941e65ede2995fadf0fb3755aedd50115dd
choi138 <kidjustinchoi@gmail.com>
2023-04-04 18:22:31 +0900

    feat: add app description

--- src/screens/main/index.tsx
+++ src/screens/main/index.tsx
@@ -8,11 +8,20 @@ import * as S from './styled';
 export const MainScreen: React.FC = () => {
   const navigate = useNavigation().navigate as (s: string) => void;
   return (
-    <S.Wrap>
-      <S.Title>Main Screen</S.Title>
+    <S.Container>
+      <S.TitleWrap>
+        <S.Title>Main Screen</S.Title>
+      </S.TitleWrap>
+      <S.DescriptionWrap>
+        <S.Description>
+          Welcome to our app, where you can easily organize your day with a to-do list feature and
+          stay up-to-date on the latest weather information. Plan your day and stay prepared for any
+          weather conditions, all in one convenient place.
+        </S.Description>
+      </S.DescriptionWrap>
       <Button title="Go to Weather" onPress={() => navigate('Weather')} />
       <Button title="Go to ToDo" onPress={() => navigate('ToDo')} />
       <StatusBar style="dark" />
-    </S.Wrap>
+    </S.Container>
   );
 };
--- src/screens/main/styled.ts
+++ src/screens/main/styled.ts
@@ -1,18 +1,35 @@
 import styled from 'styled-components/native';
 
-export const Wrap = styled.View`
+export const Container = styled.View`
   flex: 1;
   flex-direction: column;
   background-color: tomato;
-  text-align: center;
   justify-content: flex-start;
   padding-top: 100;
 `;
 
+export const TitleWrap = styled.View`
+  border-top-width: 1px;
+  border-bottom-width: 1px;
+  border-color: white;
+  align-items: center;
+  padding: 10px 0;
+`;
+
 export const Title = styled.Text`
-  text-align: center;
-  font-size: 48;
+  font-size: 48px;
   font-weight: 300;
-  /* border-top: 1px solid white;
-  border-bottom: 1px solid white; */
+`;
+
+export const DescriptionWrap = styled.View`
+  padding: 10px 0;
+  align-items: center;
+  justify-content: center;
+  border-bottom-width: 1px;
+  border-color: white;
+`;
+
+export const Description = styled.Text`
+  font-size: 24px;
+  font-weight: 300;
 `;
